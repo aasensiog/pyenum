d1d7835f2262ed1ef1f8a36a7e4c86fb05d0d983
Albert Asensio Gimenez <aasensio@gaa9.com>
2014-04-10 17:30:33 +0200

    first aprox

---
+++ enum.py
@@ -0,0 +1,29 @@
+
+from collections import namedtuple
+
+class CustomNamedTuple(namedtuple('Enumeration', 'id short desc')):
+	def __eq__(self, other):
+		if self.id:
+			return self.id == other
+		return self == other
+
+class Enum(object):
+	@classmethod
+	def choices(self):
+		d = self.__dict__
+		return tuple([(d[x].id, d[x].desc) for x in [y for y in d if not y.startswith('__')]])
+
+class Color(Enum):
+	RED = CustomNamedTuple('R', 'Red', 'Red color')
+	BLUE = CustomNamedTuple('B', 'Blue', 'Blue color')
+
+if __name__ == '__main__':
+
+	print 'B' in [Color.BLUE, Color.BLUE.id] 	# True
+	print 'R' == Color.RED 						# True
+	print 'G' in (Color.BLUE,) 					# False
+
+	print '\n'
+	print 'Converting to tuple:'
+	print Color.choices()
+
